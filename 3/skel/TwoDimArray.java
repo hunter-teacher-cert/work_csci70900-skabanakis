f9949e41e414161e6021f60d3a0abaa83c379af6
skabanakis <s.kabanakis@gmail.com>
2021-07-01 13:59:07 -0400

    marisa L victoria steph

--- 3/skel/TwoDimArray.java
+++ 3/skel/TwoDimArray.java
@@ -1,4 +1,9 @@
-/** skeleton file for
+/** 
+authors: 
+
+vmiller77, skabanakis, mlaks23
+
+skeleton file for
  class twoDimArray
   ...practice working with 2D arrays
 */
@@ -9,60 +14,110 @@ public class TwoDimArray
   //print each row of 2D integer array a on its own line,
   // using a FOR loop
   public static void print1( int[][] a ) {
-
+   for(int row = 0; row < a.length; row++){ //changed i to row
+     for(int col = 0; col < a[0].length; col++){ //changes j to col
+       System.out.print(a[row][col]); //print out each element
+     }
+     System.out.println(""); //print a line for spacing
+   }
   }
 
 
   //print each row of 2D integer array a on its own line,
   // using a FOREACH loop
   public static void print2( int[][] a ) {
-	return ;
+    for(int[] row: a){  //going through each row
+      for(int element: row){ //going through each element in a row
+        System.out.print(element);
+      }
+      System.out.println("");
+    }   
   }
 
 
   //return sum of all items in 2D integer array a
   public static int sum1( int[][] a ) {
-    return  0;
+    int total =0;
+	  int value =0;
+    for(int row =0; row<a.length; row++)
+    {
+      for(int col =0; col<a[0].length;col++)
+      {
+        value =a[row][col];
+        total= total +value; //adding each element to our total
+      }
+    }
+    return total;
   }
 
-
+/*
   //return sum of all items in 2D integer array a
   // using helper fxn sumRow
   public static int sum2( int [][] m ) {
-    return 0;
+    //loop through each row and take the sumRow
+    //then add the sumRows together
+
+    
+      double total = 0;
+      for (int[] innerArray : m)
+      {
+         for (int val : innerArray)
+         {
+            total = total + val;
+         }
+      }
+      return 0;//total / (a.length * a[0].length);
   }
 
-
+*/
   //return sum of all items on row r of 2D integer array a
   // using a FOR loop
   public static int sumRow( int r, int[][] a ) {
-	return  0;
+    int sumRowTotal= 0;
+      for(int col=0; col<a[r].length; col++){
+        sumRowTotal = sumRowTotal + a[r][col];
+      }
+  return sumRowTotal;
   }
 
 
   //return sum of all items on row r of 2D integer array a
   // using a FOREACH loop
   public static int sumRow2( int r, int[][] m ) {
-	return 0 ;
+    return 0;
   }
 
 
   public static void main( String [] args )
   {
-		/* ~~~v~~~~~slide~me~down~as~you~test~~~~~~~~~~~~~~~v~~~
+		
        int [][] m1 = new int[4][2];
        int [][] m2 = { {2,4,6}, {3,5,7} };
-       int [][] m3 = { {2}, {4,6}, {1,3,5} };
+       //int [][] m3 = { {2}, {4,6}, {1,3,5} };
+
+       System.out.println("Print1 Array m1");
        print1(m1);
+       System.out.println("Print 1 Array m2");
        print1(m2);
-       print1(m3);
+       //System.out.println("Printing m3");
+       //print1(m3);
+      
+       System.out.println("Print2 Array m1");
        print2(m1);
+       System.out.println("Print2 Array m2");
        print2(m2);
-       print2(m3);
+       //print2(m3);
+       
+       
        System.out.print("testing sum1...\n");
        System.out.println("sum m1 : " + sum1(m1));
        System.out.println("sum m2 : " + sum1(m2));
-       System.out.println("sum m3 : " + sum1(m3));
+
+      System.out.print("testing sumRow...\n");
+      System.out.println("sumRow for row 1 of m2 : " + sumRow(1,m2));
+
+       /* ~~~v~~~~~slide~me~down~as~you~test~~~~~~~~~~~~~~~v~~~
+       //System.out.println("sum m3 : " + sum1(m3));
        System.out.print("testing sum2...\n");
        System.out.println("sum m1 : " + sum2(m1));
        System.out.println("sum m2 : " + sum2(m2));
