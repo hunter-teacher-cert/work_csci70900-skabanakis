df442bdc263719dfdb6d009bdfc396b73b3585b5
skabanakis <s.kabanakis@gmail.com>
2021-07-08 13:52:58 -0400

    a combination of Michelle, Dwayne and class demos

--- 6/SuperArrayDriver.java
+++ 6/SuperArrayDriver.java
@@ -15,25 +15,30 @@ public class SuperArrayDriver
     
     System.out.println("Testing isEmpty (should be true)");
     System.out.println(sa.isEmpty());
+	
+    System.out.println("\nTesting add at index 5, 0 and 17(end)");
+    sa.add(5, 100);
+    System.out.println(sa.toString());
+    sa.add(0, 100);
+    System.out.println(sa.toString());
+    sa.add(17, 100);
+    System.out.println(sa.toString());
+	System.out.println("Testing overfill (calling grow() in add)");
+    for (int i=0; i<15; i++) {
+      sa.add(i);
+    }//for i
     /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     // -----------------------------------------------------------
     // YOUR HOMEGROWN/HOUSEMADE/ROLL-YOUR-OWN TEST METHODS HERE...
     // 
     -----------------------------------------------------------
     //precondition: newly-instantiated SuperArray of default capacity
-    System.out.println("Testing overfill (calling grow() in add)");
+   System.out.println("Testing overfill (calling grow() in add)");
     for (int i=0; i<15; i++) {
       sa.add(i);
     }//for i
     System.out.println(sa);
     System.out.println(sa.isEmpty());
-    System.out.println("\nTesting add at index 5, 0 and 17(end)");
-    sa.add(5, 100);
-    System.out.println(sa);
-    sa.add(0, 100);
-    System.out.println(sa);
-    sa.add(17, 100);
-    System.out.println(sa);
     System.out.println("\nTesting remove at 0, 5, and 15 (end)");
     sa.remove(0);
     System.out.println(sa);
