80499129e962e33d6c28c4f3795b938c3daef24e
skabanakis <s.kabanakis@gmail.com>
2021-07-16 11:30:49 -0400

    Lyuba, Julian, steph

--- ds/LIsts/Driver.java
+++ ds/LIsts/Driver.java
@@ -1,7 +1,7 @@
+//intro to linked lists
+//lyuba, julian and steph
 import java.io.*;
 import java.util.*;
-
-// victoria , eric, steph
 
 public class Driver{
     public static void main(String[] args) {
@@ -21,45 +21,6 @@ public class Driver{
 	n.getNext().setNext(new Node("Steph"));
 	System.out.println(n2.getNext());
 	System.out.println(n.getNext().getNext());
-	//created the new nodes / list
-	Node L;	
-	L = new Node();
-	L.setData("a");
-	Node L2 = new Node("b");
-	Node L3 = new Node("c");
-	Node L4 = new Node("d");
-	System.out.println(L);
-	System.out.println(L2);
-	System.out.println(L3);
-	System.out.println(L4);
-	
-	// Set up the order
-	L.setNext(L2);
-	L2.setNext(L3);
-	L3.setNext(L4);
-	
-	System.out.println(L.getNext());
-	//insert x between b & c
-	Node x;
-	x = new Node("x");
-	x.setNext(L3);
-	L2.setNext(x);
-	System.out.print(L2.getNext() + "-->");
-	System.out.println(x.getNext());
-	
-	//delete c from the mix
-	//to have x point to d. therefore we remove c.
-	x.setNext(L4);
-	System.out.print(L2.getNext() + "-->");
-	System.out.println(x.getNext() + "-->");
-	
-	System.out.println("Final Link List" );
-	System.out.print(L + " -->" + L.getNext() + "-->" + L.getNext().getNext() + "-->" + L.getNext().getNext().getNext() + "-->" + L.getNext().getNext().getNext().getNext() );
-	
-	
-	
-	
-	
 	
 	// 1.Create a new  list that looks like:
 	//   L->"a"->"b"->"c"->"d"
@@ -67,6 +28,122 @@ public class Driver{
 	//    between the b and the c
 	// 3. Write the code to delete the c
 
+
+	//1
+	Node L;
+	Node a = new Node("a");
+	Node b = new Node("b");
+	Node c = new Node("c");
+	Node d = new Node("d");
+	Node e = new Node("e");
+
+	L=a;
+	a.setNext(b);
+	b.setNext(c);
+	c.setNext(d);
+	d.setNext(e);
+	System.out.println("Part 1:");
+	// remember not to lose the head / start
+	// of the list - make a copy
+	Node currentNode;
+	currentNode = L;
+	while (currentNode != null){
+	    System.out.print(currentNode+"->");
+	    // this is like i=i+1 is for arrays
+	    // but for linked lists 
+	    currentNode = currentNode.getNext();
+	}
+	System.out.println("null");
+
+	//2
+	Node x = new Node("X");
+	x.setNext(c);
+	b.setNext(x);
+	System.out.println("Part 2:");
+	currentNode = L;
+	while (currentNode != null){
+	    System.out.print(currentNode+"->");
+	    // this is like i=i+1 is for arrays
+	    // but for linked lists 
+	    currentNode = currentNode.getNext();
+	}
+	System.out.println("null");
+
+	//3
+	b.setNext(d);
+	currentNode = L;
+	while (currentNode != null){
+	    System.out.print(currentNode+"->");
+	    // this is like i=i+1 is for arrays
+	    // but for linked lists 
+	    currentNode = currentNode.getNext();
+	}
+	System.out.println("null");
+
+	/* Or:
+	   L = new Node("a");
+	   L.setNext(new Node("B"));
+	   L.getNext().setNext(new Node("C"));
+	   L.getNext().getNext().setNext(new Node("D"));
+	   etc
+	*/
+
+	System.out.println("\n");
+	System.out.println("Llist");
+	System.out.println("-----------");
 	
+	Llist ll = new Llist();
+
+	ll.addFront("a");
+  ll.addFront("b");
+  ll.addFront("c");
+  ll.addFront("d");
+  System.out.println("The list: " + ll);
+  System.out.println(ll.get(0));
+  System.out.println(ll.get(2));
+  System.out.println(ll.get(3));
+  System.out.println(ll.get(4));
+  System.out.println(ll.get(5));
+	
+	ll.set(1, "g");
+	System.out.println("After set g at 1: " + ll);
+
+	ll.set(0, "h");
+	System.out.println("After set h at 0: " + ll);
+
+	ll.set(3, "i");
+	System.out.println("After set i at 3: " + ll);	
+
+	ll.set(4, "j");
+	System.out.println("After set j at 4: " + ll); 
+  
+  System.out.println("Test search");
+   //test search
+   ll.search("i");
+  System.out.println("Search for I: " +  ll.search("i") );
+   ll.search("j");
+  System.out.println("Search for J: " +  ll.search("j") );
+  System.out.println("Test Insert");
+  //Test Insert
+  ll.insert(2,"s");
+  System.out.println("After insert s at 2: " + ll);
+  ll.insert(7,"f");
+  System.out.println("After insert f at 7: " + ll);
+  ll.insert(0,"z");
+  System.out.println("After insert z at 0: " + ll);
+
+//test remove
+System.out.println("Test remove");
+  ll.remove(1);
+	System.out.println("After remove at 1: " + ll);
+	
+	ll.remove(0);
+	System.out.println("After remove at 0: " + ll);
+	
+	ll.remove(7);
+	System.out.println("After remove at 7: " + ll);
+	
+	ll.remove(4);
+	System.out.println("After remove at 4: " + ll);
     }
 }
--- ds/LIsts/Llist.java
+++ ds/LIsts/Llist.java
@@ -1,39 +1,226 @@
-  
 import java.io.*;
 import java.util.*;
-
+//lyuba, julian and steph
 
 public class Llist{
-  private Node front; // the front of the list
+    private Node front; // the front of the list
+    private int size; //how many items there are in the list
+                      //TODO update size every time we add or delete
+    public  Llist(){
+	    front = null;
+      size = 0;
+    }
 
-
-  public  Llist(){
-    front = null;
-  }
-
-  // Add a new node containing data
-  // at the front of the list
- // Add a new node containing data
+    // Add a new node containing data
     // at the front of the list
     public void addFront(String data){
-  	   // make the new node
-  	   Node n = new Node(data);
-  	  // point it to what front points to
-      n.setNext(front.getNext());
-    	// point front to the new node
-    	front.setNext(n);
+	// make the new node
+	Node newNode = new Node(data);
+	// point it to what front points to
+	newNode.setNext(front);
+	// point front to the new node
+	front = newNode;
+    size++;
     }
-  public String toString(){
-    Node currentNode;
-    currentNode = front;
-    String result = "";
-    while (currentNode != null){
-      result = result + currentNode + " --> " ;
-      currentNode = currentNode.getNext();
+
+    public String toString(){
+	Node currentNode;
+	currentNode = front;
+	String result = "";
+	while (currentNode != null){
+	    result = result + currentNode + "->";
+	    // this is like i=i+1 is for arrays
+	    // but for linked lists 
+	    currentNode = currentNode.getNext();
+	}
+	result = result + "null";
+	return result;
+
     }
-    result = result + "null";
-    return result;
 
-  }
+    // returns True if there is nothing in the list
+    // False otherwise
+    public boolean isEmpty(){
+      if(front == null){
+        return true;
+      }else {
+        return false;  
+      }
+      //other solution: return front == null
+    }
 
+    // returns the number of items in the list
+    // Hint: look at the toString
+    // to remind you how to traverse down the list
+    public int length(){
+	    return size;
+    }
+
+    // returns the item at location index;
+    // returns null if there aren't enough
+    // items. Starts with index 0
+    public String get(int index){
+      if(index < size) //search through list for value
+      {
+          Node n = front;
+          int i = 0; 
+          while( i < index )
+          {
+            n = n.getNext();
+            i++;
+          }
+          return n.getData();
+      }
+	    return null;
+	
+    }
+
+    // sets the item at location index (starting
+    // with 0) to value.
+    // only sets if the index is within range
+    /**
+    Node n //new node
+    Node current = //the one deleting
+    Node prev = //the node before the one we're deleting
+    while loop to find prev and current
+    prev.setNext(n);
+    n.setNext(current.getNext());
+*/
+    public void set(int index, String value){
+      //System.out.println("index " + index + " length() " + length());
+      if(index<0 || index >=length())
+      {
+        System.out.println("Exception index not valid.");
+        return;
+      }
+      int i =0;
+      Node m = new Node(value);
+      Node currentNode =front;
+      Node previousNode= null;
+      while(i< index){
+        
+         previousNode=currentNode;
+        /*
+        if(i== index){
+          currentNode.setData(value);
+          break;
+        }
+        */
+        i++;
+        currentNode =currentNode.getNext();
+      }
+      //currentNode now points to the node at the index we want to set
+      //previous node points to the node right before that index
+      if(previousNode == null) //setting at position 0
+	    {
+		    previousNode = m;
+		    front = m;
+	    }
+	    else
+	    {
+		    previousNode.setNext(m);
+	    }
+	    m.setNext(currentNode.getNext());
+      
+    }
+
+    // insert an item before index.
+    // only inserts if the index is within bounds
+    // Hint: look at toString for hints on
+    // traversal and draw out a diagram.
+    // You will need a variable that refers to
+    // the node BEFORE you want to do the insertion.
+
+    //TODO update size
+    public void insert(int index, String value){
+    if(index<0 || index >=length())
+      {
+        System.out.println("We cannot insert that "+ value + " at "+ index);
+        return;
+      }
+      int i =0;
+      Node m = new Node(value);
+      Node currentNode =front;
+      Node previousNode= null;
+      while(i< index){
+        
+         previousNode=currentNode;
+        
+        i++;
+        currentNode =currentNode.getNext();
+      }
+      
+      if(previousNode == null) //setting at position 0
+	    {
+		    previousNode = m;
+		    front = m;
+	    }
+	    else
+	    {
+		    previousNode.setNext(m);
+	    }
+      //this method is similat to our set however instead of replacing the current node we are just inserting the node and shifting the point
+	    m.setNext(currentNode);
+      size ++;
+        
+      
+    }
+
+    // returns the index of the first item with
+    // data value key. Returns -1 if not found
+    public int search(String key){
+      Node s= front;// not a new node, this node points to the front  
+      for(int i=0; i< size; i++) //loop through every element
+      {
+        if(s.getData().equals(key)) //the element is equal to key
+        //object comparisons using .equals()
+        {
+          return i; //return the index i 
+        }
+        //this will change the index at which we are comparing the string too
+        s=s.getNext();
+      }
+	      return -1;	//return -1
+    }
+    // removes the node at index.
+    
+
+    public void remove(int index){
+    //do nothing if index is out of bounds
+    if(index<0 || index >=length())
+      {
+        System.out.println("We cannot remove from index " + index);
+        return;
+      }
+
+	  Node currentNode =front;
+    Node previousNode= null;
+    int i =0;
+
+      while(i< index){
+        
+         previousNode=currentNode;
+        
+        i++;
+        currentNode =currentNode.getNext();
+      }
+      
+      if(previousNode == null) //setting at position 0
+	    {
+		    //previousNode = m;
+		    //front = m;
+        front = currentNode.getNext(); //to discuss
+        previousNode = currentNode.getNext(); //to discuss
+	    }
+	    else
+	    {
+		    //previousNode.setNext(m);
+        //bypass current node
+        previousNode.setNext(currentNode.getNext()); //to discuss
+	    }
+  //End code from insert
+  size--;
+    }
+
+    
 }
