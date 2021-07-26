54956dd9cd07482271e99fee28993ee3cfc189cc
skabanakis <s.kabanakis@gmail.com>
2021-07-25 20:58:47 -0400

    trying da trees

--- ds/Trees/part1/BSTree.java
+++ ds/Trees/part1/BSTree.java
@@ -7,13 +7,92 @@ public class BSTree {
     public BSTree(){
 	root = null;
     }
+	private void traverse(TreeNode current){
+		if(current ==null){
+			return;
+			
+		}
+		//process the current node
+		System.out.print(current.getData()+ ",");
+		
+		traverse(current.getLeft());
+		traverse(current.getRight());
+		
+			
+	}//end of private traverse
+	
+	public void traverse(){
+		traverse(root);
+	}//end of traverse
+	
+    private void preorderTraverse(TreeNode current){
+	if (current == null)
+	    return;
+
+	//process the current node
+	System.out.print(current.getData()+", ");
+
+	// recursively print out the left subtree
+	preorderTraverse(current.getLeft());
+
+	// recursively print out the right subtree
+	preorderTraverse(current.getRight());
+    }// end of preorderTraverse with node
+
+    public void preorderTraverse(){
+	preorderTraverse(root);
+	System.out.println();
+    } //end of preorderTraverse without node
+
+    private void postorderTraverse(TreeNode current){
+	if (current == null)
+	    return;
+
+	// recursively print out the left subtree
+	postorderTraverse(current.getLeft());
+
+	// recursively print out the right subtree
+	postorderTraverse(current.getRight());
+
+	//process the current node
+	System.out.print(current.getData()+", ");
+
+
+
+    }// end of postorderTraverse with node
+
+    public void postorderTraverse(){
+	postorderTraverse(root);
+	System.out.println();
+    } //end of postorderTraverse without node
+
+    private void inorderTraverse(TreeNode current){
+	if (current == null)
+	    return;
+
+
+	// recursively print out the left subtree
+	inorderTraverse(current.getLeft());
+
+	//process the current node
+	System.out.print(current.getData()+", ");
+
+	// recursively print out the right subtree
+	inorderTraverse(current.getRight());
+    }// end of inorderTraverse with nodes
+
+    public void inorderTraverse(){
+	inorderTraverse(root);
+	System.out.println();
+    } //end of 	inorderTraverse without parameters
+	
 	public int search(int key){
 	TreeNode current =root;
 		while( current != null){
-		int currentValue = current.getValue();
+		int currentValue = current.getData();
 		if(currentValue == key){
 			//if we found a match we want to return the match
-			return key;
+			return currentValue;
 		}	//end of if
 		else if(currentValue < key){
 			//if the key is higher than the root, then we are moving to the right
@@ -27,8 +106,53 @@ public class BSTree {
 			
 		}//end else
 		} //end of while
-		return -1;
+		throw new NullPointerException();
 	}//end of search
+
+ public void insert(int key){
+
+	TreeNode newNode = new TreeNode(key);
+
+	// if the tree is empty
+	// manually insert the new node as the root
+	if (root==null){
+	    root = newNode;
+	    return;
+	}
+	
+	TreeNode front = root;
+	//have to initalize the node otherwise the program will not work
+	TreeNode trailer = root;
+	
+	while (front != null){
+	    int frontValue = front.getData();
+	    if (frontValue == key){
+		// if we're here, it means the key is
+		// already in the tree so we can
+		// update this node in some way
+		// and then return
+		return;
+	    } else if (frontValue < key){
+		trailer = front;
+		front = front.getRight();
+	    } else {
+		trailer = front;
+		front = front.getLeft();
+	    }
+	}
+	if (key > trailer.getData()){
+	    // insert on the right
+		//greater than the original node
+	    trailer.setRight(newNode);
+	} else {
+	    // insert on left
+		//because its less then the value in the node
+	    trailer.setLeft(newNode);
+
+	}//end of if statements
+
+    }//end of insert
+
 
     public void seed(){
 	TreeNode t;
--- ds/Trees/part1/Driver.java
+++ ds/Trees/part1/Driver.java
@@ -3,7 +3,35 @@ import java.util.*;
 
 
 public class Driver {
-    public static void main(String[] args) {
-	BSTree t = new BSTree();
-    }
+	public static void main(String[] args) {
+		BSTree t = new BSTree();
+
+		/*t.seed();
+		
+		int value;
+		value = t.search(10);
+		System.out.println(value);
+		value = t.search(15);
+		System.out.println(value);
+		try {
+			value = t.search(17);
+			System.out.println(value);
+		} catch (NullPointerException e) {
+			System.out.println("17 not in tree");
+		}*/
+		t.insert(10);
+		t.insert(20);
+		t.insert(5);
+		t.insert(7);
+		t.insert(8);
+		t.insert(3);
+		t.insert(25);
+		
+		t.traverse();
+
+		/*t.preorderTraverse();
+		t.postorderTraverse();
+		t.inorderTraverse();*/
+		
+	}
 }
